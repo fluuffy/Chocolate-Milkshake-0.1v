5c7f59e045198bbbdf1ba0c12213ab6e539fad9e
Numan Jan <numanj4n@gmail.com>
2015-11-06 20:48:44 -0600

    Create SatelliteReceiver.h

---
+++ 16-bit-Embedded-RC-Decoder/SatelliteReceiver.h
@@ -0,0 +1,32 @@
+#ifndef SPECTRUMRECEIVER_H_
+#define SPECTRUMRECEIVER_H_
+
+#define RXCENTER 511
+#define RXTRAVEL 358 // (511 center - 63 trim) / 1,25 maxTravel
+#define RXMIN 0//153 // 511center - 358travel
+#define RXMAX 1022//869 // 511center + 358travel
+
+#include "Arduino.h"
+
+class SatelliteReceiver{
+public:
+	// void regByte(int byte);
+	void getFrame(void);
+	int getErrors();
+	int getBindType();
+	int getThro();
+	int getAile();
+	int getElev();
+	int getRudd();
+	int getGear();
+	int getFlap();
+	int getAux2();
+
+private:
+	int inData[16];
+	int index;
+	int inByte;
+};
+
+
+#endif /* SPECTRUMRECEIVER_H_ */
